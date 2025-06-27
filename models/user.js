ae00716c65023ccbf127991f2a889451712f25f6
Krishnakant-Mane <krishnamane712c@gmail.com>
2025-06-27 13:49:48 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,7 +1,12 @@
 const mongoose = require(`mongoose`)
 
-mongoose.connect(`mongodb://127.0.0.1:27017/testapp1`)
-
+//mongoose.connect(`mongodb://127.0.0.1:27017/testapp1`)
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log("✅ MongoDB connected"))
+.catch(err => console.error("❌ MongoDB connection error:", err));
 const userSchema = mongoose.Schema({
     image: String,
     name: String,
